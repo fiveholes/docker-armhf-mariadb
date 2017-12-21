0ac0fe006ccd7282e0c0afa8c2e3ec3a4875e074
fiveholes <33194117+fiveholes@users.noreply.github.com>
2017-12-21 21:56:02 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE=fiveholes/armhf-alpine-mariadb
+BUILD_TIME=$(date --rfc-2822)
+
+echo "[[ Build ${DOCKER_IMAGE} docker image ]]"
+docker build --build-arg BUILD_TIME="$BUILD_TIME" \
+             --tag ${DOCKER_IMAGE}:latest \
+             .
+
+docker push ${DOCKER_IMAGE}:latest
